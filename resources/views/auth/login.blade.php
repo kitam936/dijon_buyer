4ce4f9222dfaab2bf97a75a03d3d96e0073d17c4
kitam936 <tk.ar145qv@gmail>
2025-06-20 17:21:07 +0900

    250520_login画面修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,12 +33,12 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+            {{-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
-
+            @endif --}}
+            <span class="text-sm">パスワードを忘れた場合は <span  class="text-indigo-600 items-center"><a href="mailto:buyer-support@dijon1988.net">　buyer-support@dijon1988.net　</a></span><br>に連絡してください</span>
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
